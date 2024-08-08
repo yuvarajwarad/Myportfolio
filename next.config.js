c33cd29a54ae3bf79496da28b39acb0cf6dc8843
yuvaraj.shrimant <yuvaraj.shrimant@starhealth.in>
2024-08-08 16:43:16 +0530

    updated everthing

--- next.config.js
+++ next.config.js
@@ -1,17 +1,29 @@
-/**
- * @type {import('next').NextConfig}
- */
+/** @type {import('next').NextConfig} */
+const colors = require('tailwindcss/colors');
+
 const nextConfig = {
-  output: 'export',
- 
-  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  // trailingSlash: true,
- 
-  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  // skipTrailingSlashRedirect: true,
- 
-  // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
-}
- 
-module.exports = nextConfig
+  images: {
+    // Keep this option if you want to use image optimization
+  },
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        primary: colors.purple,
+        secondary: colors.pink,
+        ...colors,
+      },
+    },
+  },
+  plugins: [],
+};
+
+module.exports = nextConfig;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,30 @@
-/** @type {import('tailwindcss').Config} */
-const colors=require("tailwindcss/colors")
-module.exports = {
+/** @type {import('next').NextConfig} */
+const colors = require('tailwindcss/colors');
+
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true, // Disable image optimization
+  },
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        primary: colors.purple,
+        secondary: colors.pink,
+        ...colors,
       },
     },
-    colors:{
-       primary:colors.purple,
-       secondary:colors.pink,
-      ...colors,
-      
-    }
   },
   plugins: [],
 };
+
+module.exports = nextConfig;
