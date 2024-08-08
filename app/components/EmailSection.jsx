1092060e9acbda304c8313d22b16edb9c61e1084
yuvaraj.shrimant <yuvaraj.shrimant@starhealth.in>
2024-08-08 15:11:22 +0530

    updated everthing

--- app/components/EmailSection.jsx
+++ app/components/EmailSection.jsx
@@ -9,28 +9,15 @@ import Image from 'next/image';
 const EmailSection = () => {
   const [emailSubmitted, setEmailSubmitted] = useState(false);
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     const data = {
       email: e.target.email.value,
       subject: e.target.subject.value,
       message: e.target.message.value,
     };
-    const JSONdata = JSON.stringify(data);
-    const endpoint = "/api/send";
-    const options = {
-      method: "POST",
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSONdata,
-    };
-    const response = await fetch(endpoint, options);
-    const resData = await response.json();
-    if (response.status === 200) {
-      console.log('Message Sent');
-      setEmailSubmitted(true);
-    }
+    console.log('Form submitted:', data);
+    setEmailSubmitted(true);
   };
 
   return (
